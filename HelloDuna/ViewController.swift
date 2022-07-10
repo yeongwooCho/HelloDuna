17851185905ff4442953426a5b0d992d06a45b76
yeongwoo_Cho <jyy0223@naver.com>
2022-07-10 15:14:33 +0900

    [Feat] profile ui 구현

--- HelloDuna/ViewController.swift
+++ HelloDuna/ViewController.swift
@@ -8,10 +8,245 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var closedButton: UIButton = {
+        let button = UIButton()
+        let buttonImage = UIImage(named: "profileCloseBtn")
+        button.setImage(buttonImage, for: .normal)
+        return button
+    }()
+    
+    private lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "profile")
+        return imageView
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "듀나"
+        label.font = UIFont.systemFont(ofSize: 22.0, weight: .medium)
+        label.textColor = .white
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "⛴포항항"
+        label.font = UIFont.systemFont(ofSize: 15.0, weight: .medium)
+        label.textColor = .white
+        return label
+    }()
+    
+    private lazy var dividerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.3)
+        return view
+    }()
+    
+    private lazy var contentStackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [chatView, editView, storyView])
+        stack.axis = .horizontal
+        stack.alignment = .center
+        stack.distribution = .fillEqually
+        return stack
+    }()
+    
+    private lazy var chatView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    private lazy var chatButton: UIButton = {
+        let button = UIButton()
+        let buttonImage = UIImage(named: "profileTalkImg")
+        button.setImage(buttonImage, for: .normal)
+        return button
+    }()
+    private lazy var chatLabel: UILabel = {
+        let label = UILabel()
+        label.text = "나와의 채팅"
+        label.font = UIFont.systemFont(ofSize: 0, weight: .medium)
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    private lazy var editView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    private lazy var editButton: UIButton = {
+        let button = UIButton()
+        let buttonImage = UIImage(named: "profileEditImg")
+        button.setImage(buttonImage, for: .normal)
+        return button
+    }()
+    private lazy var editLabel: UILabel = {
+        let label = UILabel()
+        label.text = "프로필 편집"
+        label.font = UIFont.systemFont(ofSize: 0, weight: .medium)
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    private lazy var storyView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    private lazy var storyButton: UIButton = {
+        let button = UIButton()
+        let buttonImage = UIImage(named: "profileStoryImg")
+        button.setImage(buttonImage, for: .normal)
+        return button
+    }()
+    private lazy var storyLabel: UILabel = {
+        let label = UILabel()
+        label.text = "카카오스토리"
+        label.font = UIFont.systemFont(ofSize: 0, weight: .medium)
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    override func loadView() {
+        self.view = UIView()
+        
+        configureView()
+        configureAddSubViews()
+        configureConstraints()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    private func configureView() {
+        guard let view = self.view else { return }
+        
+        view.backgroundColor = UIColor(red: 0.53725490200000003, green: 0.56470588239999997, blue: 0.59215686270000001, alpha: 1)
+    }
+    
+    private func configureAddSubViews() {
+        guard let view = self.view else { return }
+        
+        view.addSubview(closedButton)
+        view.addSubview(profileImageView)
+        view.addSubview(nameLabel)
+        view.addSubview(descriptionLabel)
+        view.addSubview(dividerView)
+        
+        view.addSubview(contentStackView)
+        chatView.addSubview(chatButton)
+        chatView.addSubview(chatLabel)
+        editView.addSubview(editButton)
+        editView.addSubview(editLabel)
+        storyView.addSubview(storyButton)
+        storyView.addSubview(storyLabel)
+    }
+    
+    private func configureConstraints() {
+        guard let view = self.view else { return }
+        
+        closedButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            closedButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            closedButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 25),
+        ])
+        
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            profileImageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            profileImageView.widthAnchor.constraint(equalToConstant: 100),
+            profileImageView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 9),
+            nameLabel.centerXAnchor.constraint(equalTo: profileImageView.centerXAnchor)
+        ])
+        
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 7),
+            descriptionLabel.centerXAnchor.constraint(equalTo: profileImageView.centerXAnchor)
+        ])
+        
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            dividerView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 27),
+            dividerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            dividerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            dividerView.heightAnchor.constraint(equalToConstant: 1)
+        ])
+        
+        contentStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentStackView.topAnchor.constraint(equalTo: dividerView.bottomAnchor),
+            contentStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            contentStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            contentStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            contentStackView.heightAnchor.constraint(equalToConstant: 108)
+        ])
+        
+        chatView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            chatView.widthAnchor.constraint(equalToConstant: 44),
+            chatView.heightAnchor.constraint(equalToConstant: 44)
+        ])
+        chatButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            chatButton.centerXAnchor.constraint(equalTo: chatView.centerXAnchor),
+            chatButton.centerYAnchor.constraint(equalTo: chatView.centerYAnchor, constant: -10),
+            chatButton.widthAnchor.constraint(equalToConstant: 44),
+            chatButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+        chatLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            chatLabel.topAnchor.constraint(equalTo: chatButton.bottomAnchor),
+            chatLabel.leadingAnchor.constraint(equalTo: chatButton.leadingAnchor),
+            chatLabel.trailingAnchor
+                .constraint(equalTo: chatButton.trailingAnchor)
+        ])
+        
+        editView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            editView.widthAnchor.constraint(equalToConstant: 44),
+            editView.heightAnchor.constraint(equalToConstant: 44)
+        ])
+        editButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            editButton.centerXAnchor.constraint(equalTo: editView.centerXAnchor),
+            editButton.centerYAnchor.constraint(equalTo: editView.centerYAnchor, constant: -12),
+            editButton.widthAnchor.constraint(equalToConstant: 44),
+            editButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+        editLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            editLabel.topAnchor.constraint(equalTo: editButton.bottomAnchor),
+            editLabel.leadingAnchor.constraint(equalTo: editButton.leadingAnchor),
+            editLabel.trailingAnchor
+                .constraint(equalTo: editButton.trailingAnchor)
+        ])
+        
+        storyView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            storyView.widthAnchor.constraint(equalToConstant: 44),
+            storyView.heightAnchor.constraint(equalToConstant: 44)
+        ])
+        storyButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            storyButton.centerXAnchor.constraint(equalTo: storyView.centerXAnchor),
+            storyButton.centerYAnchor.constraint(equalTo: storyView.centerYAnchor, constant: -12),
+            storyButton.widthAnchor.constraint(equalToConstant: 44),
+            storyButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+        storyLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            storyLabel.topAnchor.constraint(equalTo: storyButton.bottomAnchor),
+            storyLabel.leadingAnchor.constraint(equalTo: storyButton.leadingAnchor),
+            storyLabel.trailingAnchor
+                .constraint(equalTo: storyButton.trailingAnchor)
+        ])
     }
     
 }
